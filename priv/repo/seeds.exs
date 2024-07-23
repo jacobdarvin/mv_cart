a72755e8b66df289b738bf4fa905d59a2f1aaa9e
Jacob Darvin <jacobisdarvin@gmail.com>
2024-07-24 00:02:55 +0800

    Update seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,15 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     MvCart.Repo.insert!(%MvCart.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias MvCart.Catalog
+
+Catalog.create_product(%{
+  name: "Product 1",
+  description: "Description 1",
+  quantity: 10,
+  price: 100.00
+})
+
+Catalog.create_product(%{
+  name: "Product 2",
+  description: "Description 2",
+  quantity: 5,
+  price: 50.00
+})
