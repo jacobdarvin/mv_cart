3706c493e2681353c787d0c629e1f98fbd0b702a
Jacob Darvin <jacobisdarvin@gmail.com>
2024-07-25 00:31:25 +0800

    Cleanup of unnecessary methods

--- lib/mv_cart/accounts.ex
+++ lib/mv_cart/accounts.ex
@@ -11,8 +11,6 @@ defmodule MvCart.Accounts do
 
   def get_user!(id), do: Repo.get!(User, id)
 
-  def get_user_by_email(email), do: Repo.get_by(User, email: email)
-
   def authenticate_user(email, password) do
     user = Repo.get_by(User, email: email)
 
@@ -29,7 +27,6 @@ defmodule MvCart.Accounts do
     end
   end
 
-  # Create user for wallet feature
   def create_user_with_wallet(attrs \\ %{}) do
     Repo.transaction(fn ->
       user = create_user!(attrs)
@@ -48,99 +45,5 @@ defmodule MvCart.Accounts do
     %Wallet{}
     |> Wallet.changeset(%{user_id: user_id})
     |> Repo.insert!()
-  end
-
-  @doc """
-  Returns the list of wallets.
-
-  ## Examples
-
-      iex> list_wallets()
-      [%Wallet{}, ...]
-
-  """
-  def list_wallets do
-    Repo.all(Wallet)
-  end
-
-  @doc """
-  Gets a single wallet.
-
-  Raises `Ecto.NoResultsError` if the Wallet does not exist.
-
-  ## Examples
-
-      iex> get_wallet!(123)
-      %Wallet{}
-
-      iex> get_wallet!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_wallet!(id), do: Repo.get!(Wallet, id)
-
-  @doc """
-  Creates a wallet.
-
-  ## Examples
-
-      iex> create_wallet(%{field: value})
-      {:ok, %Wallet{}}
-
-      iex> create_wallet(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_wallet(attrs \\ %{}) do
-    %Wallet{}
-    |> Wallet.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a wallet.
-
-  ## Examples
-
-      iex> update_wallet(wallet, %{field: new_value})
-      {:ok, %Wallet{}}
-
-      iex> update_wallet(wallet, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_wallet(%Wallet{} = wallet, attrs) do
-    wallet
-    |> Wallet.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a wallet.
-
-  ## Examples
-
-      iex> delete_wallet(wallet)
-      {:ok, %Wallet{}}
-
-      iex> delete_wallet(wallet)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_wallet(%Wallet{} = wallet) do
-    Repo.delete(wallet)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking wallet changes.
-
-  ## Examples
-
-      iex> change_wallet(wallet)
-      %Ecto.Changeset{data: %Wallet{}}
-
-  """
-  def change_wallet(%Wallet{} = wallet, attrs \\ %{}) do
-    Wallet.changeset(wallet, attrs)
   end
 end
--- lib/mv_cart/sales.ex
+++ lib/mv_cart/sales.ex
@@ -10,102 +10,14 @@ defmodule MvCart.Sales do
   alias MvCart.Sales.WalletTransaction
   alias MvCart.Accounts.Wallet
 
-  @doc """
-  Returns the list of purchases.
-
-  ## Examples
-
-      iex> list_purchases()
-      [%Purchase{}, ...]
-
-  """
-  def list_purchases do
-    Repo.all(Purchase)
-  end
-
   def list_user_purchases(user_id) do
     Repo.all(from p in Purchase, where: p.user_id == ^user_id)
   end
 
-  @doc """
-  Gets a single purchase.
-
-  Raises `Ecto.NoResultsError` if the Purchase does not exist.
-
-  ## Examples
-
-      iex> get_purchase!(123)
-      %Purchase{}
-
-      iex> get_purchase!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_purchase!(id), do: Repo.get!(Purchase, id)
-
-  @doc """
-  Creates a purchase.
-
-  ## Examples
-
-      iex> create_purchase(%{field: value})
-      {:ok, %Purchase{}}
-
-      iex> create_purchase(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_purchase(attrs \\ %{}) do
     %Purchase{}
     |> Purchase.changeset(attrs)
     |> Repo.insert()
-  end
-
-  @doc """
-  Updates a purchase.
-
-  ## Examples
-
-      iex> update_purchase(purchase, %{field: new_value})
-      {:ok, %Purchase{}}
-
-      iex> update_purchase(purchase, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_purchase(%Purchase{} = purchase, attrs) do
-    purchase
-    |> Purchase.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a purchase.
-
-  ## Examples
-
-      iex> delete_purchase(purchase)
-      {:ok, %Purchase{}}
-
-      iex> delete_purchase(purchase)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_purchase(%Purchase{} = purchase) do
-    Repo.delete(purchase)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking purchase changes.
-
-  ## Examples
-
-      iex> change_purchase(purchase)
-      %Ecto.Changeset{data: %Purchase{}}
-
-  """
-  def change_purchase(%Purchase{} = purchase, attrs \\ %{}) do
-    Purchase.changeset(purchase, attrs)
   end
 
   def top_up_wallet(user_id, amount) when amount > 0 do
@@ -113,14 +25,9 @@ defmodule MvCart.Sales do
       wallet = Repo.get_by!(Wallet, user_id: user_id)
       IO.inspect(wallet, label: "Wallet")
 
-      transaction =
-        %WalletTransaction{}
-        |> WalletTransaction.changeset(%{wallet_id: wallet.id, amount: amount})
-        |> Repo.insert()
-
-      IO.inspect(transaction, label: "Transaction")
-
-      transaction
+      %WalletTransaction{}
+      |> WalletTransaction.changeset(%{wallet_id: wallet.id, amount: amount})
+      |> Repo.insert()
     end)
   end
 
@@ -143,97 +50,9 @@ defmodule MvCart.Sales do
     end
   end
 
-  @doc """
-  Returns the list of wallet_transactions.
-
-  ## Examples
-
-      iex> list_wallet_transactions()
-      [%WalletTransaction{}, ...]
-
-  """
-  def list_wallet_transactions do
-    Repo.all(WalletTransaction)
-  end
-
-  @doc """
-  Gets a single wallet_transaction.
-
-  Raises `Ecto.NoResultsError` if the Wallet transaction does not exist.
-
-  ## Examples
-
-      iex> get_wallet_transaction!(123)
-      %WalletTransaction{}
-
-      iex> get_wallet_transaction!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_wallet_transaction!(id), do: Repo.get!(WalletTransaction, id)
-
-  @doc """
-  Creates a wallet_transaction.
-
-  ## Examples
-
-      iex> create_wallet_transaction(%{field: value})
-      {:ok, %WalletTransaction{}}
-
-      iex> create_wallet_transaction(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_wallet_transaction(attrs \\ %{}) do
     %WalletTransaction{}
     |> WalletTransaction.changeset(attrs)
     |> Repo.insert()
-  end
-
-  @doc """
-  Updates a wallet_transaction.
-
-  ## Examples
-
-      iex> update_wallet_transaction(wallet_transaction, %{field: new_value})
-      {:ok, %WalletTransaction{}}
-
-      iex> update_wallet_transaction(wallet_transaction, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_wallet_transaction(%WalletTransaction{} = wallet_transaction, attrs) do
-    wallet_transaction
-    |> WalletTransaction.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a wallet_transaction.
-
-  ## Examples
-
-      iex> delete_wallet_transaction(wallet_transaction)
-      {:ok, %WalletTransaction{}}
-
-      iex> delete_wallet_transaction(wallet_transaction)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_wallet_transaction(%WalletTransaction{} = wallet_transaction) do
-    Repo.delete(wallet_transaction)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking wallet_transaction changes.
-
-  ## Examples
-
-      iex> change_wallet_transaction(wallet_transaction)
-      %Ecto.Changeset{data: %WalletTransaction{}}
-
-  """
-  def change_wallet_transaction(%WalletTransaction{} = wallet_transaction, attrs \\ %{}) do
-    WalletTransaction.changeset(wallet_transaction, attrs)
   end
 end
