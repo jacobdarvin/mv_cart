b32ba72c525a3e8aefe79da21d1346bf31549015
Jacob Darvin <jacobisdarvin@gmail.com>
2024-07-25 02:11:27 +0800

    Update purchase_json.ex

--- lib/mv_cart_web/controllers/purchase_json.ex
+++ lib/mv_cart_web/controllers/purchase_json.ex
@@ -1,16 +1,10 @@
 defmodule MvCartWeb.PurchaseJSON do
   alias MvCart.Sales.Purchase
 
-  @doc """
-  Renders a list of purchases.
-  """
   def index(%{purchases: purchases}) do
     %{data: for(purchase <- purchases, do: data(purchase))}
   end
 
-  @doc """
-  Renders a single purchase.
-  """
   def show(%{purchase: purchase}) do
     %{data: data(purchase)}
   end
