8b8ff094a2efac022245e43ef2c1a5e64d515f3f
Jacob Darvin <jacobisdarvin@gmail.com>
2024-07-24 09:21:50 +0800

    Login via bcrypt

--- lib/mv_cart_web/controllers/user_controller.ex
+++ lib/mv_cart_web/controllers/user_controller.ex
@@ -14,6 +14,23 @@ defmodule MvCartWeb.UserController do
     end
   end
 
-  def login(_conn, _params) do
+  def login(conn, %{"user" => %{"email" => email, "password" => password}}) do
+    case Accounts.get_user_by_email(email) do
+      nil ->
+        conn
+        |> put_status(:unauthorized)
+        |> json(%{error: "Invalid email or password"})
+
+      %User{password_hash: password_hash} = user ->
+        if Bcrypt.verify_pass(password, password_hash) do
+          conn
+          |> put_status(:ok)
+          |> render(:show, user: user)
+        else
+          conn
+          |> put_status(:unauthorized)
+          |> json(%{error: "Invalid email or password"})
+        end
+    end
   end
 end
